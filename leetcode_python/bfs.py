9dae0c74ae6ca8d75903d3b8b1be0021856bf548
xiao_min <yumin.liu.2015@gmail.com>
2015-12-10 12:18:44 -0800

    Just clear code

--- leetcode_python/bfs.py
+++ leetcode_python/bfs.py
@@ -18,30 +18,20 @@ graph2 = {'A':['B', 'C'],
 
 def bfs(graph, start):
     arr = {'A':11,'B':22,'C':33,'D':44,'E':55, 'F':66}
-    print(arr['E'])
-    print(arr[graph['A'][1]])
 
-    #visited, stack = set(), [start]
     visited, stack = [], [start]
-    #print(stack)
+
     while stack:
         vertex = stack.pop(0)
         if vertex not in visited:
-            print(visited, stack)            
             visited.extend(vertex)
             arr[vertex]=-1
-            print(arr[vertex])
-            #print(graph[vertex])
-            #print(graph[vertex] - visited)
-            for i in range(0, len(graph[vertex])):
-                print(vertex, i, graph[vertex][i])
-                time.sleep(0.05)
 
+            for i in range(0, len(graph[vertex])):
                 if(arr[graph[vertex][i]] != -1):
                     stack.extend(graph[vertex][i])
-                    print("stack", stack)
+                    print("stack", stack, "visited", visited)
 
-            #stack.extend(graph[vertex] - set(visited))
     return visited
 
 
