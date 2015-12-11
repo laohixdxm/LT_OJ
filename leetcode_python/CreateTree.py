8df228fbdc6b581c0a26f985bd72b1a699fb70cb
xiao_min <yumin.liu.2015@gmail.com>
2015-12-11 12:58:01 -0800

    #fix bug for internal node(node in internal layer)is None, add None node processing

--- leetcode_python/CreateTree.py
+++ leetcode_python/CreateTree.py
@@ -1,5 +1,9 @@
 #CreateTree helper function for Tree related problem, tested by BFS
 
+#fix bug for internal node(node in internal layer)is None, add None node processing
+#        if(NodeList[i]==None):
+#            continue    
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -8,6 +12,8 @@ class TreeNode(object):
 
 def CreateTree(NodeList):
     for i in range(0,len(NodeList)):
+        if(NodeList[i]==None):
+            continue    
         if(2*i+1<len(NodeList)):
             NodeList[i].left = NodeList[2*i+1]
         if(2*i+2<len(NodeList)):
@@ -29,6 +35,8 @@ def bfs(root):
         node = fifo.pop(0)
         print_node(node)
         if(node==None):
+            #fifo.append(None)
+            #fifo.append(None)
             continue
         fifo.append(node.left)
         fifo.append(node.right)
@@ -39,9 +47,11 @@ bfs(root1)
 
 NodeList = [TreeNode(1), TreeNode(2), TreeNode(3)]
 NodeList.extend([TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7)])
-print(NodeList)
+#print(NodeList)
 root1 = CreateTree(NodeList)
 bfs(root1)
+
+
 
 #{1,#,2,3}
 #{1,2,3,#,#,4,#,#,5}
