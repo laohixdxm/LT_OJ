2899bd574731107d5c7861ee630418090baf3be4
xiao_min <yumin.liu.2015@gmail.com>
2016-01-06 19:52:35 -0800

    #leetcode, Merge Two Sorted Lists, #hold cur(current node), next1, next2 pointer, done

--- leetcode_python/MergeTwoSorted.py
+++ leetcode_python/MergeTwoSorted.py
@@ -1,6 +1,10 @@
 #leetcode, Merge Two Sorted Lists,
 #too tricky, give up temporarily
 
+#hold cur(current node), next1, next2 pointer, done
+
+import pdb
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
@@ -9,31 +13,38 @@ class ListNode(object):
 
 
 def mergeTwoLists(l1, l2):
-    node1, node2 = l1, l2
-    if(node1.val<=node2.val):
-        cur = 1
-        #node1=node1.next
+    next1, next2, cur = None, None, None
+    if(l1.val<=l2.val):
+        cur_list = 1        
+        cur, next1, next2=l1, l1.next, l2
     else:
-        cur = 2
-        #node2=node2.next 
+        cur_list = 2
+        cur, next1, next2=l2, l1, l2.next 
 
-    while(node1!=None) and (node2!=None):
-        if(cur==1):
-            if(node1.next.val<=node2.val): 
-                node1=node1.next
+    #pdb.set_trace()
+
+    while(next1!=None) and (next2!=None):
+        if(cur_list==1):
+            if(next1.val<=next2.val): 
+                cur, next1=cur.next, next1.next
+                cur_list = 1
             else:
-                node1_next = node1.next
-                node1.next =node2
-                node1 = node1_next
-                cur = 2
-        elif(cur==2):
-            if(node2.next.val<node1.val):
-                node2=node2.next
+                cur.next, next2 = next2, next2.next
+                cur = cur.next
+                cur_list = 2
+        elif(cur_list==2):
+            if(next2.val<next1.val):
+                cur, next2=cur.next, next2.next
+                cur_list = 2
             else:
-                node2_next = node2.next
-                node2.next = node1
-                node2 = node2_next
-                cur = 1
+                cur.next, next1 = next1, next1.next
+                cur = cur.next
+                cur_list = 1
+
+    if(next1!=None):
+        cur.next = next1
+    else:
+        cur.next = next2
 
 node1, node2, node3 = ListNode(1), ListNode(3), ListNode(5)
 node1.next, node2.next = node2, node3
@@ -42,6 +53,7 @@ node4, node5, node6 = ListNode(2), ListNode(4), ListNode(6)
 node4.next, node5.next = node5, node6
 
 mergeTwoLists(node1, node4)
+#pdb.set_trace()
 node = node1
 while(node!=None):
     print(node.val)
