eded8489fbcf61d734abb0e8dae0a4e11eb727f3
xiao_min <yumin.liu.2015@gmail.com>
2016-01-11 20:23:28 -0800

    arr = [1,5,1,1,6,4]

--- leetcode_python/WiggleSortII.py
+++ leetcode_python/WiggleSortII.py
@@ -1,10 +1,19 @@
 #leetcode WiggleSortII, initial version
+#fix bug, for test case arr = [1,5,1,1,6,4] without debugging
+
 
 import pdb
 
 # [1, 5, 1, 1, 6, 4] => [1, 4, 1, 5, 1, 6]
 # [1, 3, 2, 2, 3, 1] => [2, 3, 1, 3, 1, 2]
 
+'''
+def d(index, start1, end1, start2, end2):
+    if(start1<=index<=end1):
+        return index-start1+start2
+    if(start2<=index<=end2):
+        return index-start2+start1
+'''
 
 def merge(nums, start1, end1, start2, end2):
     if(nums[start2] > nums[end1]):
@@ -15,10 +24,24 @@ def merge(nums, start1, end1, start2, end2):
             nums[start2+i] = nums[start1+i]
             nums[start1+i] = tmp
 
+'''
+        tmp = nums[orig]
+        nums[orig] = nums[dest]
+        while(d(orig)!=dest):
+            tmp2 = nums[d(orig)]
+            nums[d(orig)] = tmp
+            
+            tmp = tmp2
+            orig = d(orig)
+        nums[dest] = tmp
+'''
+
 
 def mergeSort(nums, start, end):
     #print(start, end)
-    if(start+1==end):
+    if(start==end):
+        return
+    elif(start+1==end):
         if(nums[start] > nums[end]):
             tmp = nums[end]
             nums[end] = nums[start]
@@ -26,6 +49,7 @@ def mergeSort(nums, start, end):
         return
 
     mid = (start+end)//2
+
     #pdb.set_trace()
     mergeSort(nums, start, mid)
     mergeSort(nums, mid+1, end)
@@ -42,8 +66,10 @@ def wiggleSort(nums):
             nums[i] = tmp
 
 #arr = [2,1]
-arr = [1,2,3, 4, 5, 6, 7,8]
-#arr = [1,5,1,1,6,4]
+#arr = [1,2,3, 4, 5, 6, 7,8]
+
+arr = [1,5,1,1,6,4]
+#arr = [1,3, 2, 2, 3,1]
 #wiggleSort(arr)
 mergeSort(arr, 0, len(arr)-1)
 print(arr)
