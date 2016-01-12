9a5d393c2008202c4e2d1360581301989b4b516e
xiao_min <yumin.liu.2015@gmail.com>
2016-01-11 19:36:44 -0800

    #leetcode WiggleSortII, initial version

---
+++ leetcode_python/WiggleSortII.py
@@ -0,0 +1,58 @@
+#leetcode WiggleSortII, initial version
+
+import pdb
+
+# [1, 5, 1, 1, 6, 4] => [1, 4, 1, 5, 1, 6]
+# [1, 3, 2, 2, 3, 1] => [2, 3, 1, 3, 1, 2]
+
+
+def merge(nums, start1, end1, start2, end2):
+    if(nums[start2] > nums[end1]):
+        #switch two sub array,
+        length = end1 - start1
+        for i in range(end1 - start1+1):
+            tmp = nums[start2+i]
+            nums[start2+i] = nums[start1+i]
+            nums[start1+i] = tmp
+
+
+def mergeSort(nums, start, end):
+    #print(start, end)
+    if(start+1==end):
+        if(nums[start] > nums[end]):
+            tmp = nums[end]
+            nums[end] = nums[start]
+            nums[start] = tmp
+        return
+
+    mid = (start+end)//2
+    #pdb.set_trace()
+    mergeSort(nums, start, mid)
+    mergeSort(nums, mid+1, end)
+    merge(nums, start, mid, mid+1, end)
+
+
+def wiggleSort(nums):
+    tmp = 0
+    #
+    for i in range(0, len(nums), 2):
+        if(nums[i] > nums[i+1]):
+            tmp = nums[i+1]
+            nums[i+1] = nums[i]
+            nums[i] = tmp
+
+#arr = [2,1]
+arr = [1,2,3, 4, 5, 6, 7,8]
+#arr = [1,5,1,1,6,4]
+#wiggleSort(arr)
+mergeSort(arr, 0, len(arr)-1)
+print(arr)
+    
+    
+
+
+
+
+
+
+
